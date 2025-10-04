66fffe40b64efd8cef494ed58339cab32089cd4e
jakubkowalski <jakub.kowalski@livekid.com>
2025-10-04 13:42:30 +0200

    blank admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DemosExample from './DemosExample';
+import Admin from './pages/Admin';
 
 function App() {
     return (
-        <div>
-            <DemosExample />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DemosExample />} />
+                <Route path="/admin" element={<Admin />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
