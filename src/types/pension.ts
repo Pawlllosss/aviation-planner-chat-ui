67a9c4b2bfc8eedb3200ddf1b0f6373642f81315
Pawlllosss <oczadlypawe@gmail.com>
2025-10-05 01:30:28 +0200

    Include sick leave info in admin report

--- src/types/pension.ts
+++ src/types/pension.ts
@@ -34,6 +34,8 @@ export interface PensionRequest {
   grossSalary: number;
   retirementYear: number;
   expectedPension: number;
+  includeSickLeave: boolean;
+  avgSickDaysPerYear: number;
   zipCode?: string;
 }
 
